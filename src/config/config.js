dbe5a0ac0d9086becb6e7a69a4a0ae4b7c658be3
Freddie Smith <fs@aeonian-tech.com>
2019-04-26 20:47:51 +0200

    refactor(readme): Added instructions about how to setup APIs

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const CLOUDINARY_API = '836217542936937';
-export const CLOUDINARY_PRESET = 'aeon155';
+export const CLOUDINARY_PRESET = 'j0xdb6vx';
 export const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dh9xnvxbz/upload/';
 export const GOOGLE_API = 'AIzaSyAh1upiGYdAp3Xm4rwAHAZ3ebq51PRDyqc';
 export const GOOGLE_VISION_URL = `https://vision.googleapis.com/v1/images:annotate?key=${GOOGLE_API}`;
---
+++ src/config/example.config.js
@@ -0,0 +1,6 @@
+export const CLOUDINARY_API = ''; // Your Cloudinary API key
+export const CLOUDINARY_PRESET = ''; // Your upload preset name generated at https://cloudinary.com/console/settings/upload
+export const CLOUDINARY_URL = ''; // This should be the upload URL provided within your Cloudinary account
+export const GOOGLE_API = ''; // Your Google Cloud API key for Cloud Vision API and Maps JavaScript API.
+export const GOOGLE_VISION_URL = `https://vision.googleapis.com/v1/images:annotate?key=${GOOGLE_API}`; // No need to customise this one
+export const HEROKU_APP_URL = 'https://fys-demo.herokuapp.com'; // Originally when we inherited this legacy project, the codebase contained several references to the app running at https://fys-demo.herokuapp.com
