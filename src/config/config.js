492098e50d108dc7b33196517398c621c1b13343
Freddie Smith <fs@aeonian-tech.com>
2019-04-27 09:20:28 +0200

    refactor(example.config): created an example config for those using this project in future

--- src/config/config.js
+++
@@ -1,6 +0,0 @@
-export const CLOUDINARY_API = '836217542936937';
-export const CLOUDINARY_PRESET = 'j0xdb6vx';
-export const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dh9xnvxbz/upload/';
-export const GOOGLE_API = 'AIzaSyAh1upiGYdAp3Xm4rwAHAZ3ebq51PRDyqc';
-export const GOOGLE_VISION_URL = `https://vision.googleapis.com/v1/images:annotate?key=${GOOGLE_API}`;
-export const HEROKU_APP_URL = 'https://fys-demo.herokuapp.com';
